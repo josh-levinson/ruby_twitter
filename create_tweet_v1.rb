fd2c8c30ff8e80033cbb4dbcf33b8657929f314c
Josh Levinson <joshua.levinson@gmail.com>
2022-08-04 22:22:56 -0400

    my god it works

--- create_tweet_v1.rb
+++ create_tweet_v1.rb
@@ -1,4 +1,4 @@
-require 'byebug'
+# require 'byebug'
 require 'json'
 require 'typhoeus'
 require 'oauth'
@@ -6,6 +6,23 @@ require 'oauth/request_proxy/typhoeus_request'
 
 $LOAD_PATH << './lib'
 require 'authorizer'
+
+def create_tweet(url, oauth_params, tweet_params)
+	options = {
+	    :method => :post,
+	    headers: {
+        "User-Agent": "v1.1VUpdateStatusRuby",
+        "content-type": "application/json"
+	    },
+			params: tweet_params
+	}
+  request = Typhoeus::Request.new(url, options)
+	oauth_helper = OAuth::Client::Helper.new(request, oauth_params.merge(:request_uri => url))
+	request.options[:headers].merge!({"Authorization" => oauth_helper.header}) # Signs the request
+	response = request.run
+
+	return response
+end
 
 create_tweet_url = "https://api.twitter.com/1.1/statuses/update.json"
 
@@ -17,36 +34,13 @@ consumer_secret = ENV['CONSUMER_SECRET']
 token_key = ENV['ACCESS_TOKEN']
 token_secret = ENV['ACCESS_TOKEN_SECRET']
 
-
 # puts "Enter tweet text: "
 # tweet_text = gets.strip
-tweet_text = "plz work"
-
-# Be sure to add replace the text of the with the text you wish to Tweet.
-# You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
-@json_payload = { "status": tweet_text }
-
-def create_tweet(url, oauth_params)
-	options = {
-	    :method => :post,
-	    headers: {
-        "User-Agent": "v1.1VUpdateStatusRuby",
-        "content-type": "application/json"
-	    },
-	    body: JSON.dump(@json_payload)
-	}
-  request = Typhoeus::Request.new(url, options)
-	oauth_helper = OAuth::Client::Helper.new(request, oauth_params.merge(:request_uri => url))
-	request.options[:headers].merge!({"Authorization" => oauth_helper.header}) # Signs the request
-  byebug
-	response = request.run
-
-	return response
-end
-
+tweet_text = "Holy shit I did it I just tweeted this shit from the command-line using Ruby in Linux I am a god or at least someone who has the skills of an engineer from 8 years ago"
+tweet_params = { status: tweet_text }
 
 authorizer = Authorizer.new
 oauth_params = { consumer: authorizer.oauth_consumer, token: authorizer.oauth_token }
   
-response = create_tweet(create_tweet_url, oauth_params)
+response = create_tweet(create_tweet_url, oauth_params, tweet_params)
 puts response.code, JSON.pretty_generate(JSON.parse(response.body))
